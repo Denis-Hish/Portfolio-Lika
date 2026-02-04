331699202ecd74c90976effa8a845bf58ab59a3e
Denis-Hish <hish.denis@gmail.com>
2026-02-04 20:36:51 +0100

    Перед изменениями

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.querySelector('.nav-menu');
+const navLinks = document.querySelectorAll('.nav-link');
+const langBtns = document.querySelectorAll('.lang-btn');
+
+// Переключение меню на мобильных устройствах
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active');
+  navMenu.classList.toggle('active'); // Добавляем класс к меню
+});
+
+// Закрытие меню при клике на пункт навигации
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navToggle.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
+});
+
+// Переключение языков
+langBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    langBtns.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+  });
+});
